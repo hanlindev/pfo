91e4bf1928af5532ecaaf83e9be3baec44675236
Han Lin <hanlin.dev@gmail.com>
2013-10-20 03:23:21 +0800

    initial commit

---
+++ pfo.py
@@ -0,0 +1,12 @@
+import sqlite3
+
+class TestModel:
+	def __init__(self, num):
+		self.number = nul
+	def setNumber(self, num):
+		self.number = nul
+	def getNumber(self):
+		return self.number
+
+for name in TestModel:
+	print(name)
