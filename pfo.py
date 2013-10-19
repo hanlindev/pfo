5b720e24c9f67ca69efcd33a71573481e03b2aa4
Han Lin <hanlin.dev@gmail.com>
2013-10-20 04:19:22 +0800

    Add some more code.

--- pfo.py
+++ pfo.py
@@ -1,12 +1,30 @@
 import sqlite3
+import inspect
 
+# test dummy
 class TestModel:
 	def __init__(self, num):
-		self.number = nul
+		self.number = num
 	def setNumber(self, num):
-		self.number = nul
+		self.number = num
 	def getNumber(self):
 		return self.number
 
-for name in TestModel:
-	print(name)
+# mem = inspect.getmembers(TestModel(1))
+
+
+# for member in mem:
+# 	if inspect.ismethod(member[1]) and member[0][:3] == "set":
+# 		print member[0]
+
+# Now assume every attribute belongs to simple type
+
+class Database:
+	def __init__(self, name = None):
+		if (name is None):
+			name = "database.db"
+		self.connector = sqlite3.connect(name)
+		self.cursor = self.connector["__conn"].cursor()
+	def createTable(self, object):
+		setGetPairs = Database.__getSetGetPairs(object)
+		primaryKey = Database.__getPrimaryKey(object)
